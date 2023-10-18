32a9ce414131d165b17919d30f521d0a9a78fdae
demid <demid.krasnovidov@gmail.com>
2023-10-18 16:58:55 +0300

    Fix: working auth

--- project/src/routes/AppRouter.jsx
+++ project/src/routes/AppRouter.jsx
@@ -1,10 +1,26 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import {publicRoutes, userRoutes} from "./routes";
 import {Route, Routes} from "react-router-dom";
+import api from "../shared/service/axios/axiosClient";
 
 const AppRouter = () => {
-    // Здесь мы должы стучаться к бэку
     const [isAuth, setIsAuth] = useState(false)
+
+    const hasAccessToken = localStorage.getItem("access_token") ?? false;
+
+    useEffect(() => {
+        console.log('hasAccessToken', hasAccessToken)
+        if(hasAccessToken) {
+            const token = localStorage.getItem("access_token")
+            api.get('/auth/user', {headers: {'Authorization': `Bearer ${token}`}}).then((body) => {
+                console.log('res', body)
+                if(body?.data) {
+                    setIsAuth(true)
+                }
+            }).catch(function (error) {})
+        }
+    }, [hasAccessToken])
+
     const getRouts = () => {
         if (!isAuth) {
             return publicRoutes
