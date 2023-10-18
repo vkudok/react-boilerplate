63e79b0a67b917dc00163d83a68ff4945376cf26
VladislavShev2000 <gold64538@gmail.com>
2023-10-18 17:52:34 +0300

    Added card component

--- project/src/components/TaskCard.jsx
+++ project/src/components/TaskCard.jsx
@@ -1,17 +1,16 @@
 import * as React from 'react';
-import Box from '@mui/material/Box';
 import Card from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 
-export default function BasicCard({ title, info }) {
+export default function TaskCard({ title = 'Test', description = 'Test' }) {
   return (
     <Card sx={{ width: 250 }}>
       <CardContent>
         <Typography variant="h5" component="div">{title}</Typography>
-        <Typography variant="body2">{info}</Typography>
+        <Typography variant="body2">{description}</Typography>
       </CardContent>
       <CardActions>
         <Button size="small">Learn More</Button>
